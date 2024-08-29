e2ecd2d9175c2446bb3593a133ce6c89f6591da2
Kucharski17 <kucharski17@users.noreply.github.com>
2024-08-29 20:28:52 +0200

    second literation of refactor

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,185 +1,102 @@
+import pytest
 from pages.inventory_page import InventoryPage
 from conftest import driver, log_user
 
 
+@pytest.fixture
 def logged_in_inventory_page(driver):
     log_user(driver)
     return InventoryPage(driver)
 
 
-def clear_cart(driver):
-    inventory_page = InventoryPage(driver)
+@pytest.fixture
+def clear_cart(logged_in_inventory_page):
+    inventory_page = logged_in_inventory_page
     items = inventory_page.get_inventory_items()
+    for item in items:
+        if inventory_page.is_item_in_cart(item):
+            inventory_page.click_item_inventory_button(item)
+    yield
+    # Clear the cart after the test to ensure isolation
     for item in items:
         if inventory_page.is_item_in_cart(item):
             inventory_page.click_item_inventory_button(item)
 
 
-def test_is_inventory_displayed(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_items = inventory_page.get_inventory_items()
-
-    # Assertion: Checks if the inventory product list is not empty
+def test_is_inventory_displayed(logged_in_inventory_page):
+    inventory_items = logged_in_inventory_page.get_inventory_items()
     assert len(inventory_items) > 0, "Inventory items list is empty"
 
 
-def test_inventory_items_have_title_desc_price(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_items = inventory_page.get_inventory_items()
-
+def test_inventory_items_have_title_desc_price(logged_in_inventory_page):
+    inventory_items = logged_in_inventory_page.get_inventory_items()
     for item in inventory_items:
-        title = inventory_page.get_item_title(item)
-        desc = inventory_page.get_item_desc(item)
-        price = inventory_page.get_item_price(item)
-
-        # Assertion: Checks if each item in the inventory has a title
-        assert title, "Title is missing for one of the inventory items"
-
-        # Assertion: Checks if each item in the inventory has a description
-        assert desc, "Description is missing for one of the inventory items"
-
-        # Assertion: Checks if each item in the inventory has a price
-        assert price, "Price is missing for one of the inventory items"
+        assert logged_in_inventory_page.get_item_title(item), "Title is missing for one of the inventory items"
+        assert logged_in_inventory_page.get_item_desc(item), "Description is missing for one of the inventory items"
+        assert logged_in_inventory_page.get_item_price(item), "Price is missing for one of the inventory items"
 
 
-def test_inventory_prices_are_positive(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    items = inventory_page.get_inventory_items()
-
+def test_inventory_prices_are_positive(logged_in_inventory_page):
+    items = logged_in_inventory_page.get_inventory_items()
     for item in items:
-        price = inventory_page.get_item_price(item)
-
-        # Assertion: Checks if the product price displayed on the page is greater than 0
-        assert price > 0, f'''The price: price {price} is not greater then
-        zero for the product {inventory_page.get_item_title(item)}'''
+        price = logged_in_inventory_page.get_item_price(item)
+        assert price > 0, f"The price: {price} is not greater than zero for the product {logged_in_inventory_page.get_item_title(item)}"
 
 
-def test_inventory_prices_low_to_high(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_page.set_items_sort("Price (low to high)")
-    items = inventory_page.get_inventory_items()
-    prices = []
-
-    for item in items:
-        price = inventory_page.get_item_price(item)
-        prices.append(price)
-
-    # Assertion: Checks if the product list is sorted from lowest to highest price
+def test_inventory_prices_low_to_high(logged_in_inventory_page):
+    logged_in_inventory_page.set_items_sort("Price (low to high)")
+    items = logged_in_inventory_page.get_inventory_items()
+    prices = [logged_in_inventory_page.get_item_price(item) for item in items]
     assert prices == sorted(prices), "The product list is not sorted from lowest to highest price"
 
 
-def test_inventory_prices_high_to_low(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_page.set_items_sort("Price (high to low)")
-    items = inventory_page.get_inventory_items()
-    prices = []
-
-    for item in items:
-        price = inventory_page.get_item_price(item)
-        prices.append(price)
-
-    # Assertion: Checks if the product list is sorted from highest to lowest price
+def test_inventory_prices_high_to_low(logged_in_inventory_page):
+    logged_in_inventory_page.set_items_sort("Price (high to low)")
+    items = logged_in_inventory_page.get_inventory_items()
+    prices = [logged_in_inventory_page.get_item_price(item) for item in items]
     assert prices == sorted(prices, reverse=True), "The product list is not sorted from highest to lowest price"
 
 
-def test_inventory_name_a_to_z(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_page.set_items_sort("Name (A to Z)")
-    items = inventory_page.get_inventory_items()
-    names = []
-
-    for item in items:
-        name = inventory_page.get_item_title(item)
-        names.append(name)
-
-    # Assertion: Checks if the product list is sorted from A to Z
-    assert names == sorted(names), "he product list is not sorted alphabetically from A to Z"
+def test_inventory_name_a_to_z(logged_in_inventory_page):
+    logged_in_inventory_page.set_items_sort("Name (A to Z)")
+    items = logged_in_inventory_page.get_inventory_items()
+    names = [logged_in_inventory_page.get_item_title(item) for item in items]
+    assert names == sorted(names), "The product list is not sorted alphabetically from A to Z"
 
 
-def test_inventory_name_z_to_a(driver):
-    log_user(driver)
-
-    inventory_page = InventoryPage(driver)
-    inventory_page.set_items_sort("Name (Z to A)")
-    items = inventory_page.get_inventory_items()
-    names = []
-
-    for item in items:
-        name = inventory_page.get_item_title(item)
-        names.append(name)
-
-    # Assertion: Checks if the product list is sorted from Z to A
+def test_inventory_name_z_to_a(logged_in_inventory_page):
+    logged_in_inventory_page.set_items_sort("Name (Z to A)")
+    items = logged_in_inventory_page.get_inventory_items()
+    names = [logged_in_inventory_page.get_item_title(item) for item in items]
     assert names == sorted(names, reverse=True), "The product list is not sorted alphabetically from Z to A"
 
 
-def test_inventory_add_to_cart(driver):
-    log_user(driver)
+def test_inventory_add_to_cart(logged_in_inventory_page, clear_cart):
+    items = logged_in_inventory_page.get_inventory_items()
+    logged_in_inventory_page.click_item_inventory_button(items[0])
+    assert logged_in_inventory_page.get_item_shopping_cart_amount() == 1
 
-    inventory_page = InventoryPage(driver)
-    items = inventory_page.get_inventory_items()
+
+def test_inventory_change_button_after_add_to_cart(logged_in_inventory_page, clear_cart):
+    items = logged_in_inventory_page.get_inventory_items()
     sut = items[0]
-
-    inventory_page.click_item_inventory_button(sut)
-
-    # Assertion: Checks if the number of items in the cart is 1 after adding the first product
-    assert inventory_page.get_item_shopping_cart_amount() == 1
-
-
-def test_inventory_change_button_after_add_to_card(driver):
-    log_user(driver)
-
-    clear_cart(driver)
-
-    inventory_page = InventoryPage(driver)
-    items = inventory_page.get_inventory_items()
-    sut = items[0]
-
-    inventory_page.click_item_inventory_button(sut)
-
-    # Assertion: Checks if the button text for the first item has changed to "REMOVE"
-    button = inventory_page.get_item_inventory_button(sut)
+    logged_in_inventory_page.click_item_inventory_button(sut)
+    button = logged_in_inventory_page.get_item_inventory_button(sut)
     assert button.text == "REMOVE"
 
 
-def test_inventory_remove_item_from_card(driver):
-    log_user(driver)
-
-    clear_cart(driver)
-
-    inventory_page = InventoryPage(driver)
-    items = inventory_page.get_inventory_items()
+def test_inventory_remove_item_from_cart(logged_in_inventory_page, clear_cart):
+    items = logged_in_inventory_page.get_inventory_items()
     sut = items[0]
-
-    inventory_page.click_item_inventory_button(sut)
-    inventory_page.click_item_inventory_button(sut)
-
-    assert inventory_page.get_item_shopping_cart_amount() == 0
+    logged_in_inventory_page.click_item_inventory_button(sut)
+    logged_in_inventory_page.click_item_inventory_button(sut)
+    assert logged_in_inventory_page.get_item_shopping_cart_amount() == 0
 
 
-def test_inventory_change_after_remove_button(driver):
-    log_user(driver)
-
-    clear_cart(driver)
-
-    inventory_page = InventoryPage(driver)
-    items = inventory_page.get_inventory_items()
+def test_inventory_change_after_remove_button(logged_in_inventory_page, clear_cart):
+    items = logged_in_inventory_page.get_inventory_items()
     sut = items[0]
-
-    inventory_page.click_item_inventory_button(sut)
-    inventory_page.click_item_inventory_button(sut)
-    inventory_button = inventory_page.get_item_inventory_button(sut)
-
-    assert inventory_button.text == "ADD TO CART", ("The button did not return to 'Add to Cart' after removing the "
-                                                    "item")
+    logged_in_inventory_page.click_item_inventory_button(sut)
+    logged_in_inventory_page.click_item_inventory_button(sut)
+    inventory_button = logged_in_inventory_page.get_item_inventory_button(sut)
+    assert inventory_button.text == "ADD TO CART", "The button did not return to 'Add to Cart' after removing the item"
